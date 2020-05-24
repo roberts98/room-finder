5f1203644273396258898fd59c0efff1b5e23d46
Robert Sidzinka <robert.sidzinka@abventor.com>
2020-05-24 20:35:45 +0200

    Updates

---
+++ precache-manifest.a6e991c6a2b1ef3a560f132b01ff442b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b663f7d58874d66f4b7f7edeafafae13",
+    "url": "/room-finder/index.html"
+  },
+  {
+    "revision": "951275674c3f3c4b61d3",
+    "url": "/room-finder/static/js/2.5cea2736.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/room-finder/static/js/2.5cea2736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d887287384a148e250",
+    "url": "/room-finder/static/js/main.65158ac0.chunk.js"
+  },
+  {
+    "revision": "b29fee50e91cdd09dbb6",
+    "url": "/room-finder/static/js/runtime-main.ff6ec995.js"
+  }
+]);
